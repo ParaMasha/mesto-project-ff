9518a01b34f14ef6dbf5fee0ca658bc3104f62c3
Maria Paramonova <paramasha932224@yandex.ru>
2025-01-26 15:56:19 +0300

    cards render and deletion done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,44 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createNewCard({name, link}, deleteCallBackFunction) {
+  // Темплейт карточки
+  const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция удаления карточки
+  // Клонирование шаблона карточки
+  let cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+  // Установление значений вложенных элементов
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = name;
+
+  // Добавление к иконке удаления обработчика клика
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCallBackFunction(cardElement);
+  });
+
+  // Возвращение готового элемента карточки
+  return cardElement;
+}
+
+// Функция удаления карточки из DOM
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Отображение карточек на странице
+const placesList = document.querySelector(".places__list");
+
+function showCards(cards) {
+  cards.forEach((card) => {
+    const cardElement = createNewCard(card, deleteCard);
+    placesList.append(cardElement);
+  });
+}
+
+showCards(initialCards);
