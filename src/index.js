31c16e8edbabe556939a83f9bf3226b20b99c707
Maria Paramonova <paramasha932224@yandex.ru>
2025-03-11 22:59:11 +0300

    mesto project 7 sprint api settings 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './pages/index.css';
-import { initialCards } from './scripts/cards.js';
 import { createNewCard, deleteCard, toggleLike } from './scripts/card.js';
 import { openPopup, closePopup, closeByOverlayClick } from './scripts/modal.js';
 import { enableValidation, clearValidation } from './scripts/validation.js';
+import { getMyInfo, getInitialCards,editProfile, addCard, deleteCardById, createAvatar} from './scripts/api.js';
 
 // Объект настроек валидации
 const validationConfig = {
@@ -51,9 +51,17 @@ const cardLinkInput = document.querySelector(".popup__input_type_url");
 // Отправка формы редактирования профиля
 function submitEditProfileForm(evt) {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
-  closePopup(popupTypeEdit);
+  const newName = nameInput.value;
+  const newAbout = jobInput.value;
+  editProfile(newName, newAbout)
+    .then((updatedUserData) => {
+      profileName.textContent = updatedUserData.name;
+      profileDescription.textContent = updatedUserData.about;
+      closePopup(popupTypeEdit);
+    })
+    .catch((err) => {
+      console.log(`Ошибка при обновлении профиля: ${err}`);
+    });
 };
 
 // Слушатель отправки редакции профиля
@@ -62,11 +70,20 @@ profileForm.addEventListener("submit", submitEditProfileForm);
 // Добавление карточки
 function handleAddCard(evt) {
   evt.preventDefault();
-  const newCard = { name: cardTitleInput.value, link: cardLinkInput.value };
-  const cardElement = createNewCard(newCard, deleteCard, openImgPopup, toggleLike);
-  placesList.prepend(cardElement);
-  cardForm.reset();
-  closePopup(popupTypeNewCard);
+  const newName = cardTitleInput.value;
+  const newLink = cardLinkInput.value;
+
+  // 1. Отправляем POST-запрос, создаём карточку на сервере
+  addCard(newName, newLink)
+    .then((cardData) => {
+      const cardElement = createNewCard(cardData, deleteCard, openImgPopup, toggleLike);
+      placesList.prepend(cardElement);
+      cardForm.reset();
+      closePopup(popupTypeNewCard);
+    })
+    .catch((err) => {
+      console.log(`Ошибка при добавлении карточки: ${err}`);
+    });
 };
 
 // Слушатель отправки на обработку новой карточки
@@ -120,7 +137,36 @@ function showCards(cards) {
   });
 };
 
-// Вызов отображения карточек
-showCards(initialCards);
+// Получение информации о пользователе
+getMyInfo()
+  .then((userData) => {
+    profileName.textContent = userData.name;
+    profileDescription.textContent = userData.about;
+  })
+  .catch((err) => {
+    console.log(`Не удалось загрузить данные пользователя: ${err}`);
+  });
 
-// Работа завершена
+  // Получение начальных карточек
+  getInitialCards()
+  .then((cards) => {
+    showCards(cards); 
+  })
+  .catch((err) => {
+    console.log(`Не удалось загрузить данные карточки: ${err}`);
+  });
+
+  // Смена аватара 
+  function submitAvatarForm(evt) {
+    evt.preventDefault();
+    const avatarURL = avatarInput.value;
+  
+    createAvatar(avatarURL)
+      .then((res) => {
+        document.querySelector('.profile__image').src = res.avatar;
+        closePopup(popupAvatar);
+      })
+      .catch((err) => {
+        console.log(`Ошибка при попытке обновления аватара: ${err}`);
+      });
+  }
